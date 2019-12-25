add389afe6e085c9f615c2ecb49c8a80ba0bd3cb
“Saurav <saurav9878@gmail.com>
2019-12-25 18:18:28 +0530

    User Login code updated

--- src/UserLogin.js
+++ src/UserLogin.js
@@ -12,10 +12,10 @@ class UserLogin extends Component {
     
     handleonChange = e => this.setState({[e.target.name]: e.target.value})
     handleonSubmit(event){
-        event.preventDefault(); //Preventing default submit of form; Removing this will cause Type Error: Failed to Fetch
-        console.log("submit called")
-        console.log(JSON.stringify({email: this.state.email,
-            password: this.state.password}))
+        event.preventDefault();
+        // console.log("submit called")
+        // console.log(JSON.stringify({email: this.state.email,
+            // password: this.state.password}))
         let url = "http://127.0.0.1:5000/login"
         fetch(url,
         {
@@ -29,8 +29,11 @@ class UserLogin extends Component {
         })
         .then(res => res.json())
         .catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response)); 
-}
+        .then(response => {
+            console.log('Success:', response)
+            // console.log(props)
+        })        
+    }
 
     render() {
         return (
